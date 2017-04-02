a7891f2e93f0abbeedd7b8c7c0d13aa5c09d824f
AfroMonkey <moisalejandro@gmail.com>
2017-04-02 18:15:52 -0500

    [IMP] Custom ActionBar on ChatActivity

--- app/src/main/java/com/afroplatypus/olinia/ChatActivity.java
+++ app/src/main/java/com/afroplatypus/olinia/ChatActivity.java
@@ -1,7 +1,11 @@
 package com.afroplatypus.olinia;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -14,6 +18,16 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setIcon(R.mipmap.ic_launcher);
+
+        LayoutInflater inflator = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflator.inflate(R.layout.chat_bar, null);
+
+        actionBar.setCustomView(v);
 
 
         mMessageRecyclerView = (ListView) findViewById(R.id.list);
